11e1410677f3c4ab4a3199aa95cd8638ddd05f6b
Levy Martins Nascimento <levymnascimento@hotmail.com>
2022-02-15 13:28:00 -0300

    Criado login e autenticação com jwt

--- src/modules/user/userController.js
+++ src/modules/user/userController.js
@@ -1,11 +1,13 @@
 import { Router } from 'express'
-import { signup } from './userService'
+import { signup, login } from './userService'
+
+const AUTH_COOKIE_NAME = 'autorizathion'
 
 const router = Router()
 router.post('/signup', (req, res) => {
   try {
-    const answer = signup(req.body)
-    res.send(answer)
+    const token = signup(req.body)
+    res.cookie( AUTH_COOKIE_NAME, token ).status(201).send()
   } catch (err) {
       if(err.message === 'email_existente')
         return res.status(400).send(err.message)
@@ -16,7 +18,17 @@ router.post('/signup', (req, res) => {
 })
 
 router.post('/login', (req, res) => {
-  res.send('LOGIN /')
+  try {
+    const token = login(req.body)
+    res.cookie( AUTH_COOKIE_NAME, token ).status(201).send()
+
+  } catch (err) {
+      if(err.message === 'email_nao_encontrado' || err.message === 'senha_incorreta') 
+        return res.status(400).send(err.message)
+
+      res.status(500).send()
+
+  }
 })
 
 export default router
